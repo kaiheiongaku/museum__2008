d4fcc390a946dada02209313df8b6e779ab3fbc3
Robert Heath <robert.b.heathii@gmail.com>
2020-09-10 10:44:40 -0500

    Write test for Museum recommend_exhibit method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -28,4 +28,29 @@ class MuseumTest < Minitest::Test
     assert_equal expected, dmns.exhibits
   end
 
+  def test_recommend_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    expected1 = [dead_sea_scrolls, gems_and_minerals]
+    actual1 = dmns.recommend_exhibits(patron_1)
+
+    expected2 = [imax]
+    actual2 = dmns.recommend_exhibits(patron_2)
+
+    assert_equal expected1, actual1
+    assert_equal expected2, actual2
+  end
+
 end

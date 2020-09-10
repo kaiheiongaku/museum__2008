7f96bd04de7db3b94905b63d930e0d144b288139
Robert Heath <robert.b.heathii@gmail.com>
2020-09-10 10:38:28 -0500

    Test and code add_exhibit

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -15,5 +15,17 @@ class MuseumTest < Minitest::Test
     assert_equal [], dmns.exhibits
   end
 
+  def test_add_exhibit
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expected = [gems_and_minerals, dead_sea_scrolls, imax]
+    assert_equal expected, dmns.exhibits
+  end
 
 end

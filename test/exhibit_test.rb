7c8abe6517900c66e85644d7ad74a7ae9bfdee5f
Robert Heath <robert.b.heathii@gmail.com>
2020-09-10 10:11:43 -0500

    Write instance and attribute tests for exhibit

---
+++ test/exhibit_test.rb
@@ -0,0 +1,16 @@
+require './lib/exhibit'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ExhibitTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance_of Exhibit, exhibit
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
+
+end

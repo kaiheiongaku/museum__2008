62674e9449a2e685529c83cadf3f07f819efbf32
Robert Heath <robert.b.heathii@gmail.com>
2020-09-10 10:24:39 -0500

    Create test for add interest

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,6 +12,17 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, patron_1
     assert_equal "Bob", patron_1.name
     assert_equal 20, patron_1.spending_money
-    assert_equal [], patron_1.interests
   end
+
+  def test_add_interest
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal [], patron_1.interests
+
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    expected = ["Dead Sea Scrolls", "Gems and Minerals"]
+    assert_equal expected, patron_1.interests
+  end
+
 end
